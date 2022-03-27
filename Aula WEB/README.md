e6715627570061050ddec6252292df93032d467d
Lucas Medeiros <92602681+Qluks@users.noreply.github.com>
2022-03-27 17:32:19 -0300

    Delete Aula WEB directory

--- Aula WEB/README.md
+++
@@ -1,2 +0,0 @@
-# Programacao-WEB
-# Programacao-WEB
--- Aula WEB/script.js
+++
@@ -1,9 +0,0 @@
-arlet("Js em outro arquivo...")
-
-function exibeMensagem(){
-    alert("OOOOOOOOOOOOOOOOOOOOOH");
-}
-
-function exibeMensagem_2(){
-    alert("VRUUUUUUUUUUUUUUUUUUUUM");
-}
